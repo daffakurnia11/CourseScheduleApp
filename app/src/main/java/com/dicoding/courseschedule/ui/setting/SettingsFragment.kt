c748ea1c2bc4397662ce96d40d4d23ba0113be25
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-07-02 21:20:53 +0700

    Fixing Theme for Dark mode and Light mode

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -2,14 +2,24 @@ package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.notification.DailyReminder
+import com.dicoding.courseschedule.util.NightMode
+import java.util.*
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         //TODO 10 : Update theme based on value in ListPreference
+        findPreference<ListPreference>(getString(R.string.pref_key_dark))?.setOnPreferenceChangeListener { preference, newTheme ->
+            val themeMode = NightMode.valueOf(newTheme.toString().uppercase(Locale.US))
+            updateTheme(themeMode.value)
+            true
+        }
         //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
     }
 
